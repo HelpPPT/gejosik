684af3268449ada6585d19f42b21efebf631a04a
bgb10 <bgb10@naver.com>
2023-04-05 10:28:20 +0900

    refactor main.py

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from pydantic import BaseModel
 from gejosik import Gejosik
 
 app = FastAPI()
-
 g = Gejosik()
 
 class Item(BaseModel):
-    vocabulary: str | None = None
+    sentence: str | None = None
 
-@app.post("/items/")
+@app.post("/gejosik/")
 def parse_item(item: Item):
-    t = g.sentence(item.vocabulary)
+    t = g.sentence(item.sentence)
     return t
